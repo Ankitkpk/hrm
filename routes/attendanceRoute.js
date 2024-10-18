d82d0209c1656f8ef27384f76060d44cd87a557a
Siddhantji <teotiasiddhant@gmail.com>
2024-10-18 17:50:14 +0530

    added routes of all attendance apis

--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const attendanceController = require('../controllers/attendanceController');
 
-router.post('/markAttendance/:employeeId', attendanceController.attendance);
+// Attendance summary
+router.get('/getAttendanceSummaryByMonth/:employeeId', attendanceController.getAttendanceSummaryByMonth);
+
+// Mark attendance
+router.post('/mark', attendanceController.markAttendance);
+//Weekly attendance
+router.get('/weeklyAttendance/:employeeId', attendanceController.getWeeklyAttendance);
+
+router.get('/getTwoMonthAttendance/:employeeId',attendanceController.getTwoMonthAttendance)
 
 module.exports = router;

384864d6d9b51374c4dfc80d92d07d0dd2eca595
Siddhantji <teotiasiddhant@gmail.com>
2024-10-18 17:37:16 +0530

    added markAttendnaace

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -27,50 +27,50 @@ const getAttendanceSummaryByMonth = async (req, res) => {
 };
 
 // Mark daily attendance
-// const markAttendance = async (req, res) => {
-//   //remove leaveType
-//   const { employeeId, date, status } = req.body;
+const markAttendance = async (req, res) => {
+  //remove leaveType
+  const { employeeId, date, status } = req.body;
 
-//   try {
-//     // Determine the current month (Month Year format)
-//     //const currentMonth = new Date().toLocaleString('default', { month: 'long' }) + ' ' + new Date().getFullYear();
-//     const currentMonth = "December 2025";
-//     // Find the attendance record for the employee in the current month
-//     let attendance = await Attendance.findOne({
-//       employee: employeeId,
-//       month: currentMonth,
-//     });
+  try {
+    // Determine the current month (Month Year format)
+    //const currentMonth = new Date().toLocaleString('default', { month: 'long' }) + ' ' + new Date().getFullYear();
+    const currentMonth = "December 2025";
+    // Find the attendance record for the employee in the current month
+    let attendance = await Attendance.findOne({
+      employee: employeeId,
+      month: currentMonth,
+    });
 
-//     // If no attendance record exists for the current month, create a new one
-//     if (!attendance) {
-//       attendance = new Attendance({
-//         employee: employeeId,
-//         month: currentMonth,
-//         totalPresent: 0,
-//         totalLeavesTaken: 0,
-//       });
-//     }
+    // If no attendance record exists for the current month, create a new one
+    if (!attendance) {
+      attendance = new Attendance({
+        employee: employeeId,
+        month: currentMonth,
+        totalPresent: 0,
+        totalLeavesTaken: 0,
+      });
+    }
 
-//     // Add the daily attendance for the new date
-//     attendance.dailyAttendance.push({ date: date || new Date(), status });
+    // Add the daily attendance for the new date
+    attendance.dailyAttendance.push({ date: date || new Date(), status });
 
-//     // Update the total present/absent days based on status
-//     if (status === "Present") {
-//       attendance.totalPresent += 1;
-//     } else if (status === "Absent") {
-//       attendance.totalLeavesTaken += 1;
-//     }
+    // Update the total present/absent days based on status
+    if (status === "Present") {
+      attendance.totalPresent += 1;
+    } else if (status === "Absent") {
+      attendance.totalLeavesTaken += 1;
+    }
 
-//     // Save the updated attendance document
-//     await attendance.save();
+    // Save the updated attendance document
+    await attendance.save();
 
-//     return res
-//       .status(200)
-//       .json({ message: "Attendance marked successfully", attendance });
-//   } catch (error) {
-//     res.status(500).json({ message: "Error marking attendance", error });
-//   }
-// };
+    return res
+      .status(200)
+      .json({ message: "Attendance marked successfully", attendance });
+  } catch (error) {
+    res.status(500).json({ message: "Error marking attendance", error });
+  }
+};
 
 // const getTwoMonthAttendance = async (req, res) => {
 //   const { employeeId } = req.params;

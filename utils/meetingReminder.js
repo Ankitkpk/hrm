6c8cab12b0e67782a75fca9149415c17b9d37986
Siddhantji <teotiasiddhant@gmail.com>
2024-10-18 17:53:42 +0530

    added utils for meetingReminder and nofitication  but need modification

---
+++ utils/meetingReminder.js
@@ -0,0 +1,14 @@
+// const {createNotification} = require('../utils/notification')
+
+// const setReminder = (data)=>{
+//     const date = new Date(data.date)
+//     date.setMinutes(date.getMinutes()-data.reminder)
+//     const time = date.setMinutes - Date.now()
+//     const body = {
+//         title:'Reminder',
+//         message:`Meeting in ${data.reminder}`
+//     }
+//     setTimeout(()=>{
+//         createNotification()
+//     },time)
+// }

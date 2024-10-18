d0551c79be50324c3cb3c6734e6fc5e867ee5d78
Siddhantji <teotiasiddhant@gmail.com>
2024-10-18 17:49:25 +0530

    added meetingModel to take reminder

--- models/meeting.model.js
+++ models/meeting.model.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose")
 
 const meetingSchema = new mongoose.Schema({
     title: { type: String, required: true },
+    reminder:{
+      type: Number,
+      required: true, // Use Date type, but we will only manipulate minutes
+    },
     participants: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -11,7 +15,6 @@ const meetingSchema = new mongoose.Schema({
       }
     ],
     startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true },
     location: { type: String },
     agenda: { type: String, trim: true },
     companyId: {

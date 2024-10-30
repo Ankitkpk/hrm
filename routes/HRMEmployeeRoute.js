d30e89108082dbfdab2f62653201048d92428ae8
Siddhantji <teotiasiddhant@gmail.com>
2024-10-30 14:11:00 +0530

    fixed route of getHrmEmployeeList

--- routes/HRMEmployeeRoute.js
+++ routes/HRMEmployeeRoute.js
@@ -25,7 +25,7 @@ router.post('/updatePassword', HRMEmployeeController.updatePassword);
  router.get('/getEmployeePayslipList',HRMEmployeeController.getEmployeePaySlipList);
  router.get('/getDesignations',HRMEmployeeController.getDesignations)
  router.put('/HrmCoreEmployeeUpdate/:id',HRMEmployeeController.HrmCoreEmployeeUpdate);
- router.get('/getHrmEmployeeDetails',HRMEmployeeController.getHrmEmployeeList);
+ router.get('/getHrmEmployeeList',HRMEmployeeController.getHrmEmployeeList);
  router.get('/getHrmEmployeeDetails/:id',HRMEmployeeController.getHrmEmployeeDetails);
 
 module.exports = router; 
